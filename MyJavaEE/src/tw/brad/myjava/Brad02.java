23bff9a92fb0b6255c9b253b345b9cc5a05c6997
admin <admin@10.0.105.82>
2019-06-30 16:32:10 +0800

    201906301700

---
+++ MyJavaEE/src/tw/brad/myjava/Brad02.java
@@ -0,0 +1,63 @@
+package tw.brad.myjava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad02")
+public class Brad02 extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("service()");
+		//super.service(req, resp);
+		doTask(req, resp);
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		System.out.println("doPost");
+	}
+	
+	private void doTask(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		System.out.println("doTask");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter writer = response.getWriter();
+		writer.append("Hello, World<br>");
+		
+		StringBuffer sb = request.getRequestURL();
+		writer.append(sb + "<br>");
+		// iso-8859-1
+		Enumeration<String> names = request.getParameterNames();
+		while (names.hasMoreElements()) {
+			String name = names.nextElement();
+			String value = request.getParameter(name);
+			
+			writer.append(name + " = " + value + "<br>");
+		}
+		
+		writer.flush();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
