0ab243256448be7e2aec72f66a8dcb443ec496dc
admin <admin@MBP.r2101.tciii.org.tw>
2019-07-20 16:35:01 +0800

    201907201634

--- MyJavaEE/src/tw/brad/myjava/Brad43.java
+++ MyJavaEE/src/tw/brad/myjava/Brad43.java
@@ -2,10 +2,12 @@ package tw.brad.myjava;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javax.servlet.AsyncContext;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,29 +19,42 @@ import javax.servlet.http.HttpServletResponse;
 		asyncSupported = true
 		)
 public class Brad43 extends HttpServlet {
-	ExecutorService executorService = Executors.newFixedThreadPool(1);
-	AsyncContext asContext;
+	ExecutorService executorService = Executors.newFixedThreadPool(10);
+	//AsyncContext asContext;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("init");
+		
+		ServletContext servletContext =  getServletContext();
+		List<AsyncContext> asyncContexts = new LinkedList<>();
+		servletContext.setAttribute("asyncs", asyncContexts);
+		
+	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		request.setCharacterEncoding("UTF-8");
 
-		asContext = request.startAsync();
-		executorService.execute(new AsyncRequest());
 		
 		out.print("end");
 	}
 
 	private class AsyncRequest implements Runnable {
+		private AsyncContext asyncContext;
+		AsyncRequest(AsyncContext asyncContext){
+			this.asyncContext = asyncContext;
+		}
 		@Override
 		public void run() {
 			try {
 				Thread.sleep(10*1000);
 				
-				PrintWriter out = asContext.getResponse().getWriter();
+				PrintWriter out = asyncContext.getResponse().getWriter();
 				out.print("Lottery:" + (int)(Math.random()*49+1));
-				asContext.complete();
+				asyncContext.complete();
 			}catch(Exception e) {
 				
 			}
